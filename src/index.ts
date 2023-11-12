6cd12c27c5dc75d484c1189acfe9a5a727f7cd1b
Jannis Metrikat <120120832+jmetrikat@users.noreply.github.com>
2023-11-12 10:36:29 +0100

    Fix

--- src/index.ts
+++ src/index.ts
@@ -3,22 +3,6 @@ import contributorsManagement from "./contributorsManagement";
 import projectManagement from "./projectManagement";
 
 export = (app: Probot) => {
-  //   app.on("issue_comment", async (context) => {
-  //     const timelineResponse = await context.octokit.issues.listEventsForTimeline({
-  //       owner: "jmetrikat",
-  //       repo: "test",
-  //       issue_number: 19,
-  //     });
-  //     console.log(timelineResponse.data);
-
-  //     const event = await context.octokit.issues.getEvent({
-  //       owner: "jmetrikat",
-  //       repo: "test",
-  //       event_id: 10509521505,
-  //     });
-  //     console.log(event.data);
-  //   });
-
   contributorsManagement(app);
   projectManagement(app);
 };
